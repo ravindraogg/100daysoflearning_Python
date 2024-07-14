b3b46a876f6c39febab1debcc890e3d736b1cb54
Ravindra S <149950829+ravindraogg@users.noreply.github.com>
2024-07-14 18:18:51 +0530

    Create Day2.py
    added day2

---
+++ Day2 conditional statements/Day2.py
@@ -0,0 +1,10 @@
+#if elif else conditional statement 
+light =input("light: ")
+if (light == "red"):
+    print("Stop")
+elif (light == "yellow"):
+    print("Start")
+elif (light == "green"):
+    print("Go") 
+else:
+    print("light broken")
