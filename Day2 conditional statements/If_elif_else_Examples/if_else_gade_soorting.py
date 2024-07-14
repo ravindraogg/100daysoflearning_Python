d00cd217b28abe8fc49c85b222494fb206db229c
Ravindra S <149950829+ravindraogg@users.noreply.github.com>
2024-07-14 18:51:56 +0530

    Create if_else_gade_soorting.py
    if_else_gade_soorting example 1

---
+++ Day2 conditional statements/If_elif_else_Examples/if_else_gade_soorting.py
@@ -0,0 +1,10 @@
+#Grade sorting
+marks = int(input("Marks: "))
+if ( marks >= 90):
+    print("A Grade")
+elif (marks >= 80 and marks < 90):
+    print("B Grade")
+elif (marks >= 70 and marks < 80):
+    print("C Grade")
+else:
+    print("F Grade")
