7799c1dac0a2e1895fab60cbc9cfc1630ec218b3
Ravindra S <149950829+ravindraogg@users.noreply.github.com>
2024-07-19 11:44:53 +0530

    Create list.py
    updated day 5 list

---
+++ Day5 List & Tuples/List/list.py
@@ -0,0 +1,17 @@
+#List 
+
+Student= ["Ravindra", 18, 2005]
+print(Student)
+
+Student.append("Male")
+print(Student)
+
+print(Student[0])
+
+print(Student[1:])
+
+#Output
+''''Ravindra', 18, 2005
+   'Ravindra', 18, 2005, 'Male'
+    Ravindra
+    18, 2005, 'Male'''''
