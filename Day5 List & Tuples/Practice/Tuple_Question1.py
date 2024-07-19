56e7fd16c6a5b5c69ca173afe44731244885eef1
Ravindra S <149950829+ravindraogg@users.noreply.github.com>
2024-07-19 11:47:45 +0530

    Add files via upload
    updated day 5 practice question of tuple

---
+++ Day5 List & Tuples/Practice/Tuple_Question1.py
@@ -0,0 +1,11 @@
+#Write a program to count the number of students with the "A" grade in the following tuple.
+
+tuple = ("C","B","A","A","C","D","A")
+print(tuple.count("A"))
+
+
+#Store the above tuple in a list and sort them from A to D
+
+list1 = list(tuple)
+list1.sort()
+print(list1)
