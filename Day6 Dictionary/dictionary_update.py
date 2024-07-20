0d8ef99fe1f4483cdd1a34e453c8919c58e9ae0a
Ravindra S <149950829+ravindraogg@users.noreply.github.com>
2024-07-20 21:38:03 +0530

    Create dictionary_update.py
    UPDATED DAY 5 (UPDATE DICTIONARY)

---
+++ Day6 Dictionary/dictionary_update.py
@@ -0,0 +1,10 @@
+menu= {
+    "Name" : "Ravindra",
+    "Class" : "BE 2nd Year",
+    "DOB" : "07/09/2005" ,
+}
+
+menu.update({"Gender":"Male"})
+print(menu)
+
+#{'Name': 'Ravindra', 'Class': 'BE 2nd Year', 'DOB': '07/09/2005', 'Gender': 'Male'}
