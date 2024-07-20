29afad5a0f9af90666fd74cf787d88f27489bd16
Ravindra S <149950829+ravindraogg@users.noreply.github.com>
2024-07-20 21:30:59 +0530

    Create dictionary_get.py
    UPDATED DAY 6 DICTIONARY GET

---
+++ Day6 Dictionary/dictionary_get.py
@@ -0,0 +1,11 @@
+menu= {
+    "Name" : "Ravindra",
+    "Class" : "BE 2nd Year",
+    "DOB" : "07/09/2005" ,
+}
+
+print(menu.get("Name"))
+print(menu.get("Name2")) #It give value as None
+
+#Ravindra
+#None
