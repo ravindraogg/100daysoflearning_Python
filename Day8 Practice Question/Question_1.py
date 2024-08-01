5a605fd04629222904872b4247fcdec09944744d
Ravindra S <149950829+ravindraogg@users.noreply.github.com>
2024-08-01 11:59:53 +0530

    Create Question_1.py
    updated day 8 question 1

---
+++ Day8 Practice Question/Question_1.py
@@ -0,0 +1,9 @@
+#Store following word meaning in dictionary
+dic1 = {
+    "table": ["a pice of furniture", "list of facts & figure"],
+    "cat" : "a small animal", 
+}
+
+print(dic1)
+
+'''{'table': ['a pice of furniture', 'list of facts & figure'], 'cat': 'a small animal'}'''
