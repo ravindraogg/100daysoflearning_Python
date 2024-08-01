0d93babd16246b1d2e59bae5922875fca2b9ea05
Ravindra S <149950829+ravindraogg@users.noreply.github.com>
2024-08-01 12:00:36 +0530

    Create Question_2.py
    updated day 8 question 2

---
+++ Day8 Practice Question/Question_2.py
@@ -0,0 +1,10 @@
+#You are given a list of subjects for students. Assume one classroom is required for 1 subjects. Home many classroom are needed by all students.
+
+set1 = {"python", "C++", "Java", "Python", "JavaScript", "Java", "python", "Java", "C++", "C"}
+
+print(type(set1))
+print(len(set1))
+
+
+'''<class 'set'>
+   6'''
