37bd7bbdfcb85137758c4fc6377448641ec35dc6
Ravindra S <149950829+ravindraogg@users.noreply.github.com>
2024-08-01 12:01:31 +0530

    Create Question_3.py
    updated day 8 question 3

---
+++ Day8 Practice Question/Question_3.py
@@ -0,0 +1,12 @@
+dic1 = {}
+
+subjects = input("Subject: ")
+marks = int(input("marks: "))
+dic1.update({subjects : marks})
+
+print(dic1)
+
+
+''''Subject: english
+    marks: 80
+    {'english': 80}'''
